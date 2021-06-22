46264693326948592c7b2eec67e6ce9c6f90f4a6
FutureHet <het30401@gmail.com>
2021-06-22 17:05:51 +0530

    Updated version

---
+++ Front-End/src/app/services/password.validator.ts
@@ -0,0 +1,9 @@
+import { AbstractControl } from "@angular/forms";
+
+export function PasswordValidator(control: AbstractControl): {[key:string]: boolean} | null {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  return password && confirmPassword && password.value !== confirmPassword.value ?
+  {'notmatched' : true}: null;
+}
--- Front-End/src/app/user/user-register/user-register.component.ts
+++ Front-End/src/app/user/user-register/user-register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validator, Validators } from '@angular/forms';
+import { PasswordValidator } from 'src/app/services/password.validator';
 
 @Component({
   selector: 'app-user-register',
@@ -9,24 +10,71 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class UserRegisterComponent implements OnInit {
 
   registerationForm!: FormGroup;
-  constructor() { }
+  user: any = {};
+  constructor(private fg: FormBuilder) { }
 
   ngOnInit() {
-    this.registerationForm = new FormGroup({
-      userName: new FormControl(null, Validators.required),
+    // this.registerationForm = new FormGroup({
+    //   userName: new FormControl(null, [Validators.required, Validators.minLength(5)]),
+    //   email: new FormControl(null, [Validators.required, Validators.email]),
+    //   password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    //   confirmPassword: new FormControl(null, [Validators.required]),
+    //   mobile:  new FormControl(null, [Validators.required, Validators.maxLength(10), Validators.minLength(10)])
+    // },
+    // {validators: PasswordValidator}
+    // );
+    this.createRegistrationForm();
+  }
+
+  createRegistrationForm() {
+    this.registerationForm = this.fg.group({
+      userName: new FormControl(null, [Validators.required, Validators.minLength(5)]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
       confirmPassword: new FormControl(null, [Validators.required]),
-      mobile:  new FormControl(null, [Validators.required, Validators.maxLength(10), Validators.minLength(10)])
-    // }, this.passwordMatchingValidation(this.registerationForm));
-    });
+      mobile:  new FormControl(null, [Validators.required, Validators.maxLength(11), Validators.minLength(10)])
+    },
+    {validators: PasswordValidator}
+    );
   }
 
-  passwordMatchingValidation(fg : FormGroup): Validators {
-    return fg.get('password')!.value === fg.get('confirmPassword')!.value ? false : { notmatched: true};
+  get userName(){
+    return this.registerationForm.get('userName') as FormControl;
+  }
+  get email(){
+    return this.registerationForm.get('email') as FormControl;
+  }
+  get password(){
+    return this.registerationForm.get('password') as FormControl;
+  }
+  get confirmPassword(){
+    return this.registerationForm.get('confirmPassword') as FormControl;
+  }
+  get mobile(){
+    return this.registerationForm.get('mobile') as FormControl;
   }
 
-  onSubmit(){
+
+  onSubmit() {
     console.log(this.registerationForm);
+    // debugger;
+    let temp = this.registerationForm.value;
+    this.user = Object.assign(this.user, this.registerationForm);
+    this.addUser(this.user, temp.email);
+  }
+
+  addUser(new_user: any, new_string: string) {
+     let users: any[] = [];
+    // if(localStorage.getItem('Users') === null) {
+    //   users = [new_user];
+    // } else{
+    //   debugger;
+
+    //   users = JSON.parse(localStorage.getItem('Users') || '{}');
+    //   // users = [new_user, ...users];
+    // }
+    debugger;
+    users.push(new_user);
+    localStorage.setItem(new_string, JSON.stringify(users.values()));
   }
 }
