5910541145c27ea5c39d85beb85c9d8b00630744
FutureHet <het30401@gmail.com>
2021-07-09 16:33:13 +0530

    Updated version

--- BackEnd/Startup.cs
+++ BackEnd/Startup.cs
@@ -28,7 +28,8 @@ namespace BackEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddDbContext<DataContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddControllers();              
             services.AddSwaggerGen(c =>
             {
